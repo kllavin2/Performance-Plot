e1c73a37864f02983d5a88d5c4fb8f3bebb92e1d
kllavin2 <kllavin2@gmail.com>
2017-05-08 22:57:35 -0400

    Update Testplot.py

--- Testplot.py
+++ Testplot.py
@@ -2,8 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import csv
-
+data=[]
 with open(r"C:\Users\John\Documents\Kristi\test.csv",'rb') as fIn:
     reader=csv.reader(fIn)
-    for line in fIn:
-        print line
+    for line in reader:
+        data.append(line)
+        
+data.remove(data[0])
+# DATA SANITIZATION
+# For each sublist in list
+    #for each element in sublist
+        # if first element, convert to datetime64
+        # else convert element to float
+
+# NUMPY ARRAYS
+# List ot lists to numpy array
+
+# PLOT DATA
+# Plot desired data
+
+
+print data
