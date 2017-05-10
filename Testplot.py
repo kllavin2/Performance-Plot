af1c21bff8f305be4b1a8e3f87cd97c4eb3be29e
kllavin2 <kllavin2@gmail.com>
2017-05-09 20:13:32 -0400

    Update Testplot.py
    need to plot data

--- Testplot.py
+++ Testplot.py
@@ -9,6 +9,15 @@ with open(r"C:\Users\John\Documents\Kristi\test.csv",'rb') as fIn:
         data.append(line)
         
 data.remove(data[0])
+
+for e1 in data:
+    for n in range(0,len(e1)):
+        if n==0:
+            e1[n]=np.datetime64(ei[n]).astype
+        else: 
+            e1[n]=float(e1[n])
+            
+      
 # DATA SANITIZATION
 # For each sublist in list
     #for each element in sublist
